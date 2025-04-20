8b7519ac8096030ac1b5f29a6cc14fb35355521e
wibul1 <wibul.s@psru.ac.th>
2025-04-20 14:52:29 +0700

    create
    (cherry picked from commit 07bbb65ef0085574b544509c359edcd29248da29)

---
+++ src/main/java/com/example/meeting_room/MeetingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.meeting_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meeting_room/MeetingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meeting_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
